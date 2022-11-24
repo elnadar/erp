601dce8f2f787614c3097f806ff56232c849261b
elnadar <mo.a9tay@gmail.com>
2022-11-24 19:46:45 +0200

    [UI] added a cemment of another colorscheme.

--- lib/utils/theme/colors.dart
+++ lib/utils/theme/colors.dart
@@ -1,5 +1,10 @@
 // Text Colors
-import 'dart:ui';
+
+import 'package:flutter/material.dart';
 
 const Color headlineColor = Color(0xFF1B1D28);
 const Color mainTextColor = Color(0xFF22215B);
+
+final mainColorScheme = ColorScheme.fromSeed(seedColor: Colors.lightBlue);
+// final mainColorScheme =
+//     ColorScheme.fromSeed(seedColor: const Color(0xff6750a4));
