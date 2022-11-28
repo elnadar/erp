ac7a7e03197ef23fde67a9468cc91ce76decded7
elnadar <mo.a9tay@gmail.com>
2022-11-28 02:55:02 +0200

    [ISSUES] Fixed materials_view by adding nasted scrol view.

--- lib/home/presentation/components/prouducts_view/components/materials_view/materials_view.dart
+++ lib/home/presentation/components/prouducts_view/components/materials_view/materials_view.dart
@@ -1,37 +1,91 @@
 import 'package:flutter/material.dart';
 
 class MaterialsView extends StatelessWidget {
-  const MaterialsView({super.key});
-
+  const MaterialsView({super.key, required this.name});
+  final String name;
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        ListView(
-          shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
-          children: List.filled(
-            20,
-            ListTile(
-              title: Text("فريون"),
-              subtitle: Text("اخر تعديل الساعة 15"),
-              trailing: Text("25ج"),
-              onTap: () {},
-            ),
+    return SafeArea(
+      top: false,
+      bottom: false,
+      child: Stack(
+        children: [
+          Builder(
+            builder: (BuildContext context) {
+              return CustomScrollView(
+                key: PageStorageKey<String>(name),
+                slivers: <Widget>[
+                  SliverOverlapInjector(
+                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
+                        context),
+                  ),
+                  SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                        return ListTile(
+                          title: Text("فريون"),
+                          subtitle: Text("اخر تعديل الساعة 15"),
+                          trailing: Text("25ج"),
+                          onTap: () {},
+                        );
+                      },
+                      childCount: 30,
+                    ),
+                  ),
+                ],
+              );
+            },
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Align(
-            alignment: AlignmentDirectional.bottomEnd,
-            child: FloatingActionButton(
-              onPressed: () {},
-              tooltip: "إضافة خامة",
-              child: const Icon(Icons.add),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Align(
+              alignment: AlignmentDirectional.bottomEnd,
+              child: FloatingActionButton(
+                onPressed: () {},
+                tooltip: "إضافة خامة",
+                child: const Icon(Icons.add),
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
+
+
+// class MaterialsView extends StatelessWidget {
+//   const MaterialsView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         ListView(
+//           shrinkWrap: true,
+//           physics: const NeverScrollableScrollPhysics(),
+//           children: List.filled(
+//             20,
+            // ListTile(
+            //   title: Text("فريون"),
+            //   subtitle: Text("اخر تعديل الساعة 15"),
+            //   trailing: Text("25ج"),
+            //   onTap: () {},
+            // ),
+//           ),
+//         ),
+        // Padding(
+        //   padding: const EdgeInsets.all(20.0),
+        //   child: Align(
+        //     alignment: AlignmentDirectional.bottomEnd,
+        //     child: FloatingActionButton(
+        //       onPressed: () {},
+        //       tooltip: "إضافة خامة",
+        //       child: const Icon(Icons.add),
+        //     ),
+        //   ),
+        // )
+//       ],
+//     );
+//   }
+// }
--- lib/home/presentation/components/prouducts_view/products_view.dart
+++ lib/home/presentation/components/prouducts_view/products_view.dart
@@ -11,40 +11,94 @@ class ProductsView extends StatelessWidget {
     // final ThemeData theme = Theme.of(context);
     return DefaultTabController(
       length: 2,
-      child: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            leading: const MenuBarIcon(),
-            actions: [
-              IconButton(
-                icon: const Icon(Icons.more_vert_rounded),
-                onPressed: () {},
-              )
-            ],
-            floating: true,
-            pinned: true,
-            snap: true,
-            stretch: true,
-            title: const Text("المنتجات"),
-            bottom: const TabBar(
-              // labelColor: theme.colorScheme.secondary,
-              tabs: [
-                Tab(
-                  text: "الخامات",
+      child: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverOverlapAbsorber(
+              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+              sliver: SliverAppBar(
+                leading: const MenuBarIcon(),
+                actions: [
+                  IconButton(
+                    icon: const Icon(Icons.more_vert_rounded),
+                    onPressed: () {},
+                  )
+                ],
+                floating: true,
+                pinned: true,
+                stretch: true,
+                forceElevated: innerBoxIsScrolled,
+                title: const Text("المنتجات"),
+                bottom: const TabBar(
+                  // labelColor: theme.colorScheme.secondary,
+                  tabs: [
+                    Tab(
+                      text: "الخامات",
+                    ),
+                    Tab(
+                      text: "المنتجات",
+                    ),
+                  ],
                 ),
-                Tab(
-                  text: "المنتجات",
-                ),
-              ],
+              ),
             ),
+          ];
+        },
+        body: const TabBarView(children: [
+          MaterialsView(
+            name: "الخامات",
           ),
-          const SliverFillRemaining(
-            child: SizedBox.expand(
-              child: TabBarView(children: [MaterialsView(), MaterialsView()]),
-            ),
-          ),
-        ],
+          MaterialsView(
+            name: "المنتجات",
+          )
+        ]),
       ),
     );
   }
 }
+
+// class ProductsView extends StatelessWidget {
+//   const ProductsView({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // final ThemeData theme = Theme.of(context);
+//     return DefaultTabController(
+//       length: 2,
+//       child: CustomScrollView(
+//         slivers: [
+//           SliverAppBar(
+//             leading: const MenuBarIcon(),
+//             actions: [
+//               IconButton(
+//                 icon: const Icon(Icons.more_vert_rounded),
+//                 onPressed: () {},
+//               )
+//             ],
+//             floating: true,
+//             pinned: true,
+//             snap: true,
+//             stretch: true,
+//             title: const Text("المنتجات"),
+//             bottom: const TabBar(
+//               // labelColor: theme.colorScheme.secondary,
+//               tabs: [
+//                 Tab(
+//                   text: "الخامات",
+//                 ),
+//                 Tab(
+//                   text: "المنتجات",
+//                 ),
+//               ],
+//             ),
+//           ),
+//           const SliverFillRemaining(
+//             child: SizedBox.expand(
+//               child: TabBarView(children: [MaterialsView(), MaterialsView()]),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
