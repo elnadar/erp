72ec8dbaeefa7d47b72541f4e07341e4003709b9
elnadar <mo.a9tay@gmail.com>
2022-12-06 23:48:39 +0200

    [VIEW] changed old sliver tab bar with the new widget.

--- lib/home/presentation/components/prouducts_view/products_view.dart
+++ lib/home/presentation/components/prouducts_view/products_view.dart
@@ -1,6 +1,6 @@
 import 'package:erp/home/presentation/components/menu_bar_icon/menu_bar_icon.dart';
 import 'package:erp/home/presentation/components/prouducts_view/components/materials_view/materials_view.dart';
-import 'package:erp/utils/localization/ar.dart';
+import 'package:erp/home/presentation/components/sliver_tab_bar/sliver_tab_bar.dart';
 import 'package:flutter/material.dart';
 
 class ProductsView extends StatelessWidget {
@@ -8,97 +8,40 @@ class ProductsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final ThemeData theme = Theme.of(context);
-    return DefaultTabController(
-      length: 2,
-      child: NestedScrollView(
-        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
-          return <Widget>[
-            SliverOverlapAbsorber(
-              handle: NestedScrollView.sliverOverlapAbsorberHandleFor(context),
-              sliver: SliverAppBar(
-                leading: const MenuBarIcon(),
-                actions: [
-                  IconButton(
-                    icon: const Icon(Icons.more_vert_rounded),
-                    onPressed: () {},
-                  )
-                ],
-                floating: true,
-                pinned: true,
-                stretch: true,
-                forceElevated: innerBoxIsScrolled,
-                title: const Text("المنتجات"),
-                bottom: const TabBar(
-                  // labelColor: theme.colorScheme.secondary,
-                  tabs: [
-                    Tab(
-                      text: "الخامات",
-                    ),
-                    Tab(
-                      text: "المنتجات",
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ];
-        },
-        body: const TabBarView(children: [
-          MaterialsView(
-            name: "الخامات",
-          ),
-          MaterialsView(
-            name: "المنتجات",
+    return SliverTabBar(
+      tabLength: 2,
+      sliverAppBar: SliverAppBar(
+        leading: const MenuBarIcon(),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.more_vert_rounded),
+            onPressed: () {},
           )
-        ]),
+        ],
+        floating: true,
+        pinned: true,
+        stretch: true,
+        title: const Text("المنتجات"),
+        bottom: const TabBar(
+          // labelColor: theme.colorScheme.secondary,
+          tabs: [
+            Tab(
+              text: "الخامات",
+            ),
+            Tab(
+              text: "المنتجات",
+            ),
+          ],
+        ),
       ),
+      body: const TabBarView(children: [
+        MaterialsView(
+          name: "الخامات",
+        ),
+        MaterialsView(
+          name: "المنتجات",
+        )
+      ]),
     );
   }
 }
-
-// class ProductsView extends StatelessWidget {
-//   const ProductsView({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // final ThemeData theme = Theme.of(context);
-//     return DefaultTabController(
-//       length: 2,
-//       child: CustomScrollView(
-//         slivers: [
-//           SliverAppBar(
-//             leading: const MenuBarIcon(),
-//             actions: [
-//               IconButton(
-//                 icon: const Icon(Icons.more_vert_rounded),
-//                 onPressed: () {},
-//               )
-//             ],
-//             floating: true,
-//             pinned: true,
-//             snap: true,
-//             stretch: true,
-//             title: const Text("المنتجات"),
-//             bottom: const TabBar(
-//               // labelColor: theme.colorScheme.secondary,
-//               tabs: [
-//                 Tab(
-//                   text: "الخامات",
-//                 ),
-//                 Tab(
-//                   text: "المنتجات",
-//                 ),
-//               ],
-//             ),
-//           ),
-//           const SliverFillRemaining(
-//             child: SizedBox.expand(
-//               child: TabBarView(children: [MaterialsView(), MaterialsView()]),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
