502736af80551172e473fd6124a5d9ffb4b73062
elnadar <mo.a9tay@gmail.com>
2022-12-06 23:49:13 +0200

    [VIEW] changed old sliver tab bar child with the new one.

--- lib/home/presentation/components/prouducts_view/components/materials_view/materials_view.dart
+++ lib/home/presentation/components/prouducts_view/components/materials_view/materials_view.dart
@@ -1,58 +1,92 @@
+import 'package:erp/home/presentation/components/sliver_tab_bar/sliver_tab_bar_scrollable_child.dart';
+import 'package:erp/home/presentation/components/sliver_tab_bar/sliver_tab_bar_scrollable_child_with_fab.dart';
 import 'package:erp/utils/theme/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class MaterialsView extends StatelessWidget {
   const MaterialsView({super.key, required this.name});
   final String name;
   @override
   Widget build(BuildContext context) {
-    final double height = MediaQuery.of(context).size.height;
-    return SafeArea(
-      top: false,
-      bottom: false,
-      child: Stack(
-        children: [
-          Builder(
-            builder: (BuildContext context) {
-              return CustomScrollView(
-                key: PageStorageKey<String>(name),
-                slivers: <Widget>[
-                  SliverOverlapInjector(
-                    handle: NestedScrollView.sliverOverlapAbsorberHandleFor(
-                        context),
-                  ),
-                  SliverList(
-                    delegate: SliverChildBuilderDelegate(
-                      (BuildContext context, int index) {
-                        return ListTile(
-                          title: Text("فريون"),
-                          subtitle: Text("اخر تعديل الساعة 15"),
-                          trailing: Text("25ج"),
-                          onTap: () {},
-                        );
-                      },
-                      childCount: 30,
-                    ),
-                  ),
-                ],
+    return SliverTabBarScrollableChildWithFab(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _sheetBuilder(context),
+        // onPressed: () => showMaterialsSheet(context),
+        tooltip: "إضافة خامة",
+        child: const Icon(Icons.add),
+      ),
+      child: SliverTabBarScrollableChild(
+        name: name,
+        child: SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (BuildContext context, int index) {
+              return ListTile(
+                title: Text("فريون"),
+                subtitle: Text("اخر تعديل الساعة 15"),
+                trailing: Text("25ج"),
+                onTap: () {},
               );
             },
+            childCount: 30,
           ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Align(
-              alignment: AlignmentDirectional.bottomEnd,
-              child: FloatingActionButton(
-                onPressed: () => showMaterialsSheet(context),
-                tooltip: "إضافة خامة",
-                child: const Icon(Icons.add),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
+
+  Future<dynamic> _sheetBuilder(BuildContext context) {
+    return showBarModalBottomSheet(
+      context: context,
+      builder: _builderOfSheetBuilder,
+    );
+  }
+
+  Widget _builderOfSheetBuilder(context) => SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+                TextFormField(),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      );
 }
 
 Future<dynamic> showMaterialsSheet(BuildContext context) {
