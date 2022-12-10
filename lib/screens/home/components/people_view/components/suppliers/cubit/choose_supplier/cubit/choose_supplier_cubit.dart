2df2e89533c0af04fba6e2be63c3665f3b6c63f0
elnadar <mo.a9tay@gmail.com>
2022-12-10 03:46:13 +0200

    .

--- lib/screens/home/components/people_view/components/suppliers/cubit/choose_supplier/cubit/choose_supplier_cubit.dart
+++ lib/screens/home/components/people_view/components/suppliers/cubit/choose_supplier/cubit/choose_supplier_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:erp/screens/home/components/people_view/components/suppliers/data/supplier_model.dart';
 import 'package:erp/screens/home/components/people_view/components/suppliers/data/suppliers_list.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 
 part 'choose_supplier_state.dart';
@@ -9,11 +10,16 @@ class ChooseSupplierCubit extends Cubit<ChooseSupplierState> {
   ChooseSupplierCubit() : super(ChooseSupplierInitial());
   SupplierModel? _model;
   SupplierModel? get model => _model;
+  int? _currentIndex;
+  int? get currentIndex => _currentIndex;
+
   choose(int index) {
     emit(ChooseSupplierLoading());
 
     try {
       _model = SuppliersList().suppliersList[index];
+      _currentIndex = index;
+      debugPrint("$_currentIndex");
       emit(ChooseSupplierDone());
     } catch (e) {
       emit(ChooseSupplierError(e.toString()));
