b106d1f86363704b77282c0a2ff68c7496604135
elnadar <mo.a9tay@gmail.com>
2022-12-10 02:58:12 +0200

    .

--- lib/screens/home/components/prouducts_view/components/materials_view/parts/bottom_sheet/bottom_sheet_form.dart
+++ lib/screens/home/components/prouducts_view/components/materials_view/parts/bottom_sheet/bottom_sheet_form.dart
@@ -12,7 +12,6 @@ class _AddMaterialForm extends StatefulWidget {
 }
 
 class _AddMaterialFormState extends State<_AddMaterialForm> {
-  WeightUnits dropdownValue = _lst.first;
   ThemeData get theme => Theme.of(context);
 
   late final GlobalKey<FormState> _formKey;
@@ -65,28 +64,23 @@ class _AddMaterialFormState extends State<_AddMaterialForm> {
             textInputAction: TextInputAction.next,
           ),
           const SizedBox(height: 16),
-          StatefulBuilder(builder: (BuildContext context, _) {
-            return DropdownButtonFormField<WeightUnits>(
-                decoration: const InputDecoration(labelText: 'وحدة القياس'),
-                value: dropdownValue,
-                items: _lst
-                    .map<DropdownMenuItem<WeightUnits>>((WeightUnits value) {
-                  return DropdownMenuItem<WeightUnits>(
-                    value: value,
-                    child: Text(getWeightUnitAr(value)),
-                  );
-                }).toList(),
-                onChanged: (val) => setState(() {
-                      dropdownValue = val!;
-                    }));
-          }),
+          const _UnitsChooser(),
           const SizedBox(height: 16),
-          TextFormField(
-            controller: _nameController,
-            decoration: const InputDecoration(labelText: "المورد"),
-            keyboardType: TextInputType.name,
-            textInputAction: TextInputAction.next,
-          ),
+          const _SuppliersChooser(),
+          // TextFormField(
+          //   controller: _nameController,
+          //   decoration: const InputDecoration(labelText: "المورد"),
+          //   keyboardType: TextInputType.name,
+          //   textInputAction: TextInputAction.next,
+          // ),
+          // SizedBox(
+          //   width: double.infinity,
+          //   child: FilledTonalButton(
+          //     onPressed: _chooseSupplier,
+          //     child: const Text("اختر مورد"),
+          //   ),
+          // ),
+
           const SizedBox(height: 16),
           TextFormField(
             minLines: 3,
@@ -109,6 +103,159 @@ class _AddMaterialFormState extends State<_AddMaterialForm> {
           ),
         ],
       ),
+    );
+  }
+}
+
+class _UnitsChooser extends StatefulWidget {
+  const _UnitsChooser({super.key});
+
+  @override
+  State<_UnitsChooser> createState() => _UnitsChooserState();
+}
+
+class _UnitsChooserState extends State<_UnitsChooser> {
+  @override
+  Widget build(BuildContext context) {
+    WeightUnits dropdownValue = _lst.first;
+
+    return DropdownButtonFormField<WeightUnits>(
+        decoration: const InputDecoration(labelText: 'وحدة القياس'),
+        value: dropdownValue,
+        items: _lst.map<DropdownMenuItem<WeightUnits>>((WeightUnits value) {
+          return DropdownMenuItem<WeightUnits>(
+            value: value,
+            child: Text(getWeightUnitAr(value)),
+          );
+        }).toList(),
+        onChanged: (val) => setState(() {
+              dropdownValue = val!;
+            }));
+  }
+}
+
+class _SuppliersChooser extends StatefulWidget {
+  const _SuppliersChooser({super.key});
+
+  @override
+  State<_SuppliersChooser> createState() => __SuppliersChooserState();
+}
+
+class __SuppliersChooserState extends State<_SuppliersChooser> {
+  ThemeData get theme => Theme.of(context);
+
+  _chooseSupplier() {
+    showCupertinoModalBottomSheet(
+      context: context,
+      barrierColor: Colors.black87,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(18.0),
+            child: CustomText(
+              'اختر المورد',
+              textStyle: theme.textTheme.titleLarge,
+            ),
+          ),
+          const Divider(
+            height: 1,
+            thickness: 1,
+          ),
+          BlocProvider<SuplliersCubit>(
+            create: (context) => SuplliersCubit(),
+            child: const Material(
+              child: _ChooseSupplierView(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    String dropdownValue = "";
+
+    return InkWell(
+      onTap: _chooseSupplier,
+      child: DropdownButtonFormField<String>(
+          decoration: const InputDecoration(labelText: 'المورد'),
+          value: "قم بالضغط لإختيار مورد",
+          items: null,
+          enableFeedback: true,
+          onChanged: (val) => setState(() {
+                dropdownValue = val!;
+              })),
+    );
+  }
+}
+
+class _ChooseSupplierView extends StatefulWidget {
+  const _ChooseSupplierView({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<_ChooseSupplierView> createState() => _ChooseSupplierViewState();
+}
+
+class _ChooseSupplierViewState extends State<_ChooseSupplierView> {
+  final _suppliersList = SuppliersList().suppliersList;
+  int? _selectedIndex;
+  late final SuplliersCubit _cubit;
+
+  @override
+  void initState() {
+    _cubit = BlocProvider.of<SuplliersCubit>(context);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_suppliersList.isEmpty) {
+      if (_cubit.state is SuplliersLoading) {
+        return const SizedBox(
+          height: 200,
+          child: Center(
+            child: CircularProgressIndicator(),
+          ),
+        );
+      } else if (_cubit.state is! SuplliersData) {
+        return const SizedBox(
+          height: 200,
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(20.0),
+              child: Text(
+                  "لا يوجد موردين حاليًا، قم بإضافة مورد ثم حاول مرة اخري"),
+            ),
+          ),
+        );
+      }
+    }
+    return ListView.builder(
+      shrinkWrap: true,
+      itemBuilder: ((context, index) {
+        SupplierModel model = _suppliersList[index];
+        return ListTile(
+          title: Text(model.name),
+          selected: index == _selectedIndex,
+          subtitle: Text(model.phoneNumber ?? ""),
+          onTap: () {
+            setState(() {
+              _selectedIndex = index;
+            });
+          },
+        );
+      }),
+      itemCount: _suppliersList.length,
     );
   }
 }
