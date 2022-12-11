fa955b19b76eace48b80eac232702d5676c464a6
elnadar <mo.a9tay@gmail.com>
2022-12-11 23:14:34 +0200

    Added keyboard support for materials form.

--- lib/screens/home/components/prouducts_view/components/materials_view/parts/bottom_sheet/bottom_sheet_builder.dart
+++ lib/screens/home/components/prouducts_view/components/materials_view/parts/bottom_sheet/bottom_sheet_builder.dart
@@ -1,12 +1,16 @@
 part of '../../materials_view.dart';
 
-Widget _builderOfSheetBuilder(context) => SingleChildScrollView(
-      controller: ModalScrollController.of(context),
-      child: BlocProvider<MaterialsAddCubit>(
-        create: (context) => MaterialsAddCubit(),
-        child: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: _AddMaterialForm(),
+Widget _builderOfSheetBuilder(context) => Padding(
+      padding:
+          EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+      child: SingleChildScrollView(
+        controller: ModalScrollController.of(context),
+        child: BlocProvider<MaterialsAddCubit>(
+          create: (context) => MaterialsAddCubit(),
+          child: const Padding(
+            padding: EdgeInsets.all(16.0),
+            child: _AddMaterialForm(),
+          ),
         ),
       ),
     );
