ea7f2e411a00a4aa24f8cd19939e1feec1ae2eee
elnadar <mo.a9tay@gmail.com>
2022-12-18 00:24:38 +0200

    created prices db file.

---
+++ lib/screens/home/components/prouducts_screen/controllers/prices_controller/data/prices_db.dart
@@ -0,0 +1,61 @@
+import 'package:erp/utils/database/init.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'price_model.dart';
+
+enum PricesType { product, material }
+
+class DbPricesTable {
+  static const String _tableName = 'Prices';
+  DbPricesTable._sharedInstance();
+  static final DbPricesTable _shared = DbPricesTable._sharedInstance();
+  factory DbPricesTable() => _shared;
+
+  Future<Database> get _db async {
+    return await DbProvider().database;
+  }
+
+  insert(PriceModel model) async {
+    final db = await _db;
+    model.copyWith(id: await db.insert(_tableName, model.toSqlInsert()));
+    return model;
+  }
+
+  listRows() async {
+    final db = await _db;
+    List<PriceModel> pricesList = [];
+    List<Map> maps = await db.query(_tableName, columns: PriceModel.columns);
+    if (maps.isNotEmpty) {
+      for (final Map<String, dynamic> map
+          in maps as List<Map<String, dynamic>>) {
+        pricesList.add(PriceModel.fromMap(map));
+      }
+      return pricesList;
+    }
+    return null;
+  }
+
+  getByType(PricesType type) async {
+    final db = await _db;
+
+    List<Map> maps = await db.query(_tableName,
+        columns: PriceModel.columns,
+        where: '${type.toString()}_id != ?',
+        whereArgs: [null]);
+    if (maps.isNotEmpty) {
+      return PriceModel.fromMap(maps.first as Map<String, dynamic>);
+    }
+    return null;
+  }
+
+  getPrice(int id) async {
+    final db = await _db;
+
+    List<Map> maps = await db.query(_tableName,
+        columns: PriceModel.columns, where: '_id = ?', whereArgs: [id]);
+    if (maps.isNotEmpty) {
+      return PriceModel.fromMap(maps.first as Map<String, dynamic>);
+    }
+    return null;
+  }
+}
