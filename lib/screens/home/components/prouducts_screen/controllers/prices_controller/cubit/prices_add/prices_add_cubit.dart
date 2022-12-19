0addf90f281863fcdd41eef50567dff55203f3c7
elnadar <mo.a9tay@gmail.com>
2022-12-19 15:34:17 +0200

    created prices add cubit.

--- lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices_add/prices_add_cubit.dart
+++ lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices_add/prices_add_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
+import 'package:erp/screens/home/components/prouducts_screen/controllers/prices_controller/data/price_model.dart';
+import 'package:erp/screens/home/components/prouducts_screen/controllers/prices_controller/data/prices_db.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 
 part 'prices_add_state.dart';
 
 class PricesAddCubit extends Cubit<PricesAddState> {
   PricesAddCubit() : super(PricesAddInitial());
+  addData(PriceModel model) async {
+    try {
+      emit(PricesAddingDataLoading());
+      await DbPricesTable().insert(model).then((value) {
+        debugPrint('$value');
+      });
+      emit(PricesAddingDataDone());
+    } catch (e) {
+      emit(PricesAddingDataError(e.toString()));
+    }
+  }
 }
--- lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices_add/prices_add_state.dart
+++ lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices_add/prices_add_state.dart
@@ -4,3 +4,14 @@ part of 'prices_add_cubit.dart';
 abstract class PricesAddState {}
 
 class PricesAddInitial extends PricesAddState {}
+
+class PricesAddingDataLoading extends PricesAddState {}
+
+class PricesAddingDataDone extends PricesAddState {}
+
+class PricesAddingDataError extends PricesAddState {
+  final String error;
+  PricesAddingDataError(this.error) {
+    debugPrint(error);
+  }
+}
