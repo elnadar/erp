eadd9cb3310689f44fe72d02bc7c7d641a6a1bf6
elnadar <mo.a9tay@gmail.com>
2022-12-19 15:20:47 +0200

    finished prices cubit

--- lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices/prices_cubit.dart
+++ lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices/prices_cubit.dart
@@ -1,8 +1,44 @@
 import 'package:bloc/bloc.dart';
+import 'package:erp/screens/home/components/prouducts_screen/controllers/prices_controller/data/prices_db.dart';
+import 'package:erp/screens/home/components/prouducts_screen/controllers/prices_controller/data/prices_list.dart';
+import 'package:flutter/foundation.dart';
 import 'package:meta/meta.dart';
 
 part 'prices_state.dart';
 
 class PricesCubit extends Cubit<PricesState> {
-  PricesCubit() : super(PricesInitial());
+  PricesCubit() : super(PricesInitial()) {
+    getMaterialsPrices();
+    getProductsPrices();
+  }
+  final PricesList _prices = PricesList();
+  final DbPricesTable _table = DbPricesTable();
+
+  Future<void> getMaterialsPrices() async {
+    emit(PricesLoading());
+    try {
+      _prices.materialsList = await _table.listRows() ?? [];
+      if (_prices.materialsList.isEmpty) {
+        emit(PricesNoDataFound());
+      } else {
+        emit(PricesData());
+      }
+    } catch (e) {
+      emit(PricesGettingDataError(e.toString()));
+    }
+  }
+
+  Future<void> getProductsPrices() async {
+    emit(PricesLoading());
+    try {
+      _prices.productsList = await _table.listRows() ?? [];
+      if (_prices.productsList.isEmpty) {
+        emit(PricesNoDataFound());
+      } else {
+        emit(PricesData());
+      }
+    } catch (e) {
+      emit(PricesGettingDataError(e.toString()));
+    }
+  }
 }
--- lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices/prices_state.dart
+++ lib/screens/home/components/prouducts_screen/controllers/prices_controller/cubit/prices/prices_state.dart
@@ -4,3 +4,16 @@ part of 'prices_cubit.dart';
 abstract class PricesState {}
 
 class PricesInitial extends PricesState {}
+
+class PricesLoading extends PricesState {}
+
+class PricesData extends PricesState {}
+
+class PricesNoDataFound extends PricesState {}
+
+class PricesGettingDataError extends PricesState {
+  final String error;
+  PricesGettingDataError(this.error) {
+    debugPrint(error);
+  }
+}
